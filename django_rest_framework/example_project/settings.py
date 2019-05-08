34de62592607f45c528835c93ceb4c5d9e792a00
Derek Wasinger <derekwasinger@gmail.com>
2019-05-07 17:05:10 -0700

    reverting hard coded test

--- django_rest_framework/example_project/settings.py
+++ django_rest_framework/example_project/settings.py
@@ -88,33 +88,22 @@ engines = {
     'mysql': 'django.db.backends.mysql',
 }
 
-
-# def config():
-#     service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
-#     if service_name:
-#         engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
-#     else:
-#         engine = engines['sqlite']
-#     name = os.getenv('DATABASE_NAME')
-#     if not name and engine == engines['sqlite']:
-#         name = os.path.join(BASE_DIR, 'db.sqlite3')
-#     return {
-#         'ENGINE': engine,
-#         'NAME': name,
-#         'USER': os.getenv('DATABASE_USER'),
-#         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-#         'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name)),
-#         'PORT': os.getenv('{}_SERVICE_PORT'.format(service_name)),
-#     }
-
 def config():
+    service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
+    if service_name:
+        engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
+    else:
+        engine = engines['sqlite']
+    name = os.getenv('DATABASE_NAME')
+    if not name and engine == engines['sqlite']:
+        name = os.path.join(BASE_DIR, 'db.sqlite3')
     return {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'default',
-        'USER': 'django',
-        'PASSWORD': 'VTHWjW8WuU4Xinfl',
-        'HOST': 'postgresql',
-        'PORT': '5432',
+        'ENGINE': engine,
+        'NAME': name,
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name)),
+        'PORT': os.getenv('{}_SERVICE_PORT'.format(service_name)),
     }
 
 DATABASES = {
