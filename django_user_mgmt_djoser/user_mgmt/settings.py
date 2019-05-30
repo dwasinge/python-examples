2d74a7e08218ba763ebba724aabd3d872e022dc2
Derek Wasinger <derekwasinger@gmail.com>
2019-05-30 09:02:20 -0700

    added default health check

--- django_user_mgmt_djoser/user_mgmt/settings.py
+++ django_user_mgmt_djoser/user_mgmt/settings.py
@@ -20,17 +20,21 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eg9ozff!__l)*x+c$vuk2i&(y=nwo(994vn*r=d5w+3%p&^7zw'
-
+SECRET_KEY = os.getenv(
+    'DJANGO_SECRET_KEY',
+    # safe value used for development when DJANGO_SECRET_KEY might not be set
+    '9e4@&tw46$l31)zrqe3wi+-slqm(ruvz&se0^%9#6(_w3ui!c0'
+)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,9 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'djoser',
+    'health',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,11 +81,42 @@ WSGI_APPLICATION = 'user_mgmt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+engines = {
+    'sqlite': 'django.db.backends.sqlite3',
+    'postgresql': 'django.db.backends.postgresql_psycopg2',
+    'mysql': 'django.db.backends.mysql',
+}
+
+def config():
+    service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
+    if service_name:
+        engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
+    else:
+        engine = engines['sqlite']
+    name = os.getenv('DATABASE_NAME')
+    if not name and engine == engines['sqlite']:
+        name = os.path.join(BASE_DIR, 'db.sqlite3')
+
+    if DEBUG:
+        user = os.getenv('DATABASE_USER')
+        password = os.getenv('DATABASE_PASSWORD')
+        host = os.getenv('{}_SERVICE_HOST'.format(service_name))
+        port = os.getenv('{}_SERVICE_PORT'.format(service_name))
+
+        print('connecting to db:')
+        print(f' engine: {engine}\n name: {name}\n user: {user} \n password: {password}\n host: {host}\n port: {port}\n')
+
+    return {
+        'ENGINE': engine,
+        'NAME': name,
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name)),
+        'PORT': os.getenv('{}_SERVICE_PORT'.format(service_name)),
     }
+
+DATABASES = {
+    'default': config()
 }
 
 REST_FRAMEWORK = {
--- django_user_mgmt_djoser/user_mgmt/urls.py
+++ django_user_mgmt_djoser/user_mgmt/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from health.views import health
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('health/', health),
 ]
