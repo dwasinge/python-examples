71eb30823b2773db040aaf98ae189bbd94264449
Derek Wasinger <derekwasinger@gmail.com>
2019-05-30 10:45:59 -0700

    api prefix change and token fix

--- django_user_mgmt_djoser/user_mgmt/urls.py
+++ django_user_mgmt_djoser/user_mgmt/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from health.views import health
 
 urlpatterns = [
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.jwt')),
     path('health/', health),
 ]
