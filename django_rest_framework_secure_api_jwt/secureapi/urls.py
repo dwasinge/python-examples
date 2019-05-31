9c7d612d48b0b9a4a225067ac813d71122a13e12
Derek Wasinger <derekwasinger@gmail.com>
2019-05-31 10:12:29 -0700

    added health url

--- django_rest_framework_secure_api_jwt/secureapi/urls.py
+++ django_rest_framework_secure_api_jwt/secureapi/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from secureapi.api.views import AuthenticatedView, AdminView
+from health.views import health
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authenticated/', AuthenticatedView.as_view(), name='authenticated'),
     path('adminview/', AdminView.as_view(), name='authenticated'),
+    path('health/', health),
 ]
