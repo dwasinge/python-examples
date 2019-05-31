6b9657d67eb8ca820045a5bdc28e4951bb61d933
Derek Wasinger <derekwasinger@gmail.com>
2019-05-31 08:05:15 -0700

    setting static root again

--- django_user_mgmt_djoser/user_mgmt/settings.py
+++ django_user_mgmt_djoser/user_mgmt/settings.py
@@ -167,6 +167,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.getenv('APP_ROOT', BASE_DIR)
+STATIC_BASE_DIR = os.getenv('APP_ROOT', BASE_DIR)
+STATIC_ROOT = f'{STATIC_BASE_DIR}/static'
 
 STATIC_URL = '/static/'
